f801813a44cf1913fe7b266e35eef9421c23f060
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-12-25 06:55:47 +0000

    Add files via upload

--- services/rss-feed-creator/startup/rss-init.js
+++ services/rss-feed-creator/startup/rss-init.js
@@ -1,6 +1,6 @@
 // services/rss-feed-creator/startup/rss-init.js
 import { ensureR2Sources } from "../utils/rss-bootstrap.js";
-import { info, error ,debug} from "#logger.js";
+import { info, error ,debug} from "../../../logger.js";
 
 (async () => {
   try {
