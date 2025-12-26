f3878793bd0582d86b7a60761a19f988ca8a1187
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-12-26 04:42:32 +0000

    Update script.js

--- routes/script.js
+++ routes/script.js
@@ -1,5 +1,5 @@
 import express from "express";
-import log from "../logger.js";
+import { info, error, debug } from "../logger.js";
 import { orchestrateScript } from "../services/script/utils/orchestrator.js";
 
 const router = express.Router();
