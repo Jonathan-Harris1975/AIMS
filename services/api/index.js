db15cfdf54129f69ddf0bca91a732695e752761a
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-12-26 04:28:53 +0000

    Update index.js

--- services/api/index.js
+++ services/api/index.js
@@ -4,14 +4,13 @@ import podcastRouter from "../podcast/index.js";
 import scriptRouter from "../script/index.js";
 import ttsRouter from "../tts/index.js";
 import artworkRouter from "../artwork/index.js";
-import mergeRouter from "../merge/index.js";
+
 
 export const router = express.Router();
 
 router.use("/podcast", podcastRouter);
 router.use("/script", scriptRouter);
 router.use("/tts", ttsRouter);
-router.use("/merge", mergeRouter);
 router.use("/artwork", artworkRouter);
 
 export default router;
