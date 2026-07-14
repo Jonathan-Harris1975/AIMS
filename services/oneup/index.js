2d0f620d424311cd6f516cbe77ded76f65c5483c
Jonathan-Harris1975 <205491236+Jonathan-Harris1975@users.noreply.github.com>
2026-07-15 00:48:08 +0100

    Delete services/oneup directory

--- services/oneup/index.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import routes from "./routes/index.js";
-
-const router = express.Router();
-router.use("/", routes);
-
-export default router;
--- services/oneup/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import socialRoutes from "./social.js";
-
-const router = express.Router();
-
-router.use("/", socialRoutes);
-
-export default router;
